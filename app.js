922f4f3845a2cdebc456b8678a6c54de5188482e
Sakina Shivji <shivjisakina@gmail.com>
2017-08-22 21:23:22 -0400

    set up files and got firebase set up

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEj5pVnkQr38EoOTHA-0uX5u0TfJO-WoE",
+    authDomain: "testing-sakina.firebaseapp.com",
+    databaseURL: "https://testing-sakina.firebaseio.com",
+    projectId: "testing-sakina",
+    storageBucket: "",
+    messagingSenderId: "484303247887"
+};
+firebase.initializeApp(config);
